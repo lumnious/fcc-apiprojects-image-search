bd48d64ed3b9f078b04b48cf2493f84eca1304a6
lumnious <dotico@gmail.com>
2016-06-23 14:11:01 +0100

    Added /latest view.

--- routes/latest.js
+++ routes/latest.js
@@ -6,7 +6,11 @@ var db = require('../public/javascripts/db.js');
 /* GET latest images page. */
 router.get('/', function(req, res, next) {
 	db.getSearches(function (val) {
-		res.send(val.body);
+		res.render('latest', {
+			title: 'Latest Searches',
+			results: val.body
+		})
+		//res.send();
 	})
 });
 
